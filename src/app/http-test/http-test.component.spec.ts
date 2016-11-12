0fed41e1f0e69ef54739c69c2608225f81ac61f1
Syed Aman <sna274@nyu.edu>
2016-11-12 16:13:25 -0500

    generate HttpTestComponent files

---
+++ src/app/http-test/http-test.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { HttpTestComponent } from './http-test.component';
+
+describe('Component: HttpTest', () => {
+  it('should create an instance', () => {
+    let component = new HttpTestComponent();
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/http-test/http-test.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-http-test',
+  templateUrl: './http-test.component.html',
+  styleUrls: ['./http-test.component.css']
+})
+export class HttpTestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
