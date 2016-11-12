078bcf9f8a6371d651bf9efd06fd16b341b7a30c
Syed Aman <sna274@nyu.edu>
2016-11-12 16:17:16 -0500

    define template for REST & rename selector

--- src/app/http-test/http-test.component.ts
+++ src/app/http-test/http-test.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-http-test',
+  selector: 'http-test',
   templateUrl: './http-test.component.html',
   styleUrls: ['./http-test.component.css']
 })
 export class HttpTestComponent implements OnInit {
+  getData: string;
+  postData: string;
 
   constructor() { }
 
